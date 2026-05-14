8d939eec37c20bac5e9490e3a9b8d6008f003e97
Halcyon <zouhao6@foxmail.com>
2026-05-14 16:41:01 +0800

    移除编译输出目录target和dist

--- frontend/dist/assets/auth-KV-4FlHU.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./request-BGHutx2P.js";const o=t=>r.post("/auth/login",t),e=t=>r.post("/auth/register",t);export{o as l,e as r};
--- frontend/dist/assets/tag-DlICHnEo.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./request-BGHutx2P.js";const a=()=>s.get("/tags"),g=()=>s.get("/user/tags"),r=e=>s.post("/user/tags",e),o=e=>s.delete(`/user/tags/${e}`);export{g as a,r as b,o as d,a as g};
